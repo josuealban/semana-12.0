7df726cd59c510343351d75e7672d28272281a7a
josuealban <mjalban@sudamericano.edu.ec>
2025-06-30 21:00:47 -0500

    actualizacion

--- src/ingredientes/ingredientes.controller.ts
+++ src/ingredientes/ingredientes.controller.ts
@@ -1,31 +1,30 @@
-import { Controller, Post, Body, Get} from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { IngredientesService } from './ingredientes.service';
 import { CreateIngredienteDto } from './dto/create-ingrediente.dto';
-import {Delete, Param, Put } from '@nestjs/common';
+import { Param, Put, Delete } from '@nestjs/common';
 import { UpdateIngredienteDto } from './dto/update-ingrediente.dto';
 
 @Controller('ingredientes')
 export class IngredientesController {
-  constructor(private readonly ingredientesService: IngredientesService) {}
+constructor(private readonly ingredientesService: IngredientesService) {}
 
-  @Post()
-  create(@Body() dto: CreateIngredienteDto) {
-    return this.ingredientesService.create(dto);
-  }
+@Post()
+create(@Body() dto: CreateIngredienteDto) {
+return this.ingredientesService.create(dto);
+}
 
-  @Get()
-  findAll() {
-    return this.ingredientesService.findAll();
-  }
-  
-  @Put(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateIngredienteDto) {
-    return this.ingredientesService.update(+id, dto);
-  }
+@Get()
+findAll() {
+return this.ingredientesService.findAll();
+}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ingredientesService.remove(+id);
-  }
-  
+@Put(':id')
+update(@Param('id') id: string, @Body() dto: UpdateIngredienteDto) {
+return this.ingredientesService.update(+id, dto);
+}
+
+@Delete(':id')
+remove(@Param('id') id: string) {
+return this.ingredientesService.remove(+id);
+}
 }
--- src/ingredientes/ingredientes.service.ts
+++ src/ingredientes/ingredientes.service.ts
@@ -7,27 +7,26 @@ import { UpdateIngredienteDto } from './dto/update-ingrediente.dto';
 
 @Injectable()
 export class IngredientesService {
-  constructor(
-    @InjectRepository(Ingrediente)
-    private ingredienteRepo: Repository<Ingrediente>,
-  ) {}
+constructor(
+@InjectRepository(Ingrediente)
+private ingredienteRepo: Repository<Ingrediente>,
+) {}
+create(dto: CreateIngredienteDto) {
+const nuevo = this.ingredienteRepo.create(dto);
+return this.ingredienteRepo.save(nuevo);
+}
 
-  create(dto: CreateIngredienteDto) {
-    const nuevo = this.ingredienteRepo.create(dto);
-    return this.ingredienteRepo.save(nuevo);
-  }
- 
-  @Get()
-  findAll() {
-    return this.ingredienteRepo.find();   
-  }
+@Get()
+findAll() {
+return this.ingredienteRepo.find();
+}
 
-   async update(id: number, dto: UpdateIngredienteDto) {
-    await this.ingredienteRepo.update(id, dto);
-     return this.ingredienteRepo.findOneBy({ id });
-  }
-  
-  remove(id: number) {
-    return this.ingredienteRepo.delete(id);
-  }
+async update(id: number, dto: UpdateIngredienteDto) {
+await this.ingredienteRepo.update(id, dto);
+return this.ingredienteRepo.findOneBy({ id });
+}
+
+remove(id: number) {
+return this.ingredienteRepo.delete(id);
+}
 }
