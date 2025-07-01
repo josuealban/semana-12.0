95c6fe14479e2720d4188b37aa9522ebddb31bb7
josuealban <mjalban@sudamericano.edu.ec>
2025-06-30 20:23:31 -0500

    Update create-ingrediente.dto.ts

--- src/ingredientes/dto/create-ingrediente.dto.ts
+++ src/ingredientes/dto/create-ingrediente.dto.ts
@@ -1,1 +1,15 @@
-export class CreateIngredienteDto {}
+import{ IsNotEmpty, IsString, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateIngredienteDto {
+@IsNotEmpty()
+@IsString()
+nombre: string;
+
+@IsNotEmpty()
+@IsNumber()
+precio: number;
+
+@IsOptional()
+@IsString()
+unidad?: string;
+}
